00d492e83c4567b6ae63b1be35f0d813a52936ac
obayomi96 <martinsoluwaseun47@gmail.com>
2019-04-10 00:46:04 +0100

    chore(validation):implement input validation upon user signup > Firstname field is required > firstname field should be letters > lastname field is required and should be letters > email field is required and must be a valid email > password field is required and must be min of 4 chars

--- api/v1/routes/userRouter.js
+++ api/v1/routes/userRouter.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import userController from '../controllers/UserController';
+import userController from '../controllers/userController';
 import inputValidator from '../middlewares/validator';
 
 const router = express.Router();
 
 // User sign up route
-router.post('/signup', inputValidator.userSignup, userController.signUp);
+router.post('/signup', inputValidator, userController.signUp);
 
 // User sign in route
 router.post('/signin', userController.signIn);
