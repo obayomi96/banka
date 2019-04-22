d85250dea471ca05f6904861efe8c85d803427d2
obayomi96 <martinsoluwaseun47@gmail.com>
2019-04-22 01:52:36 +0100

    chore(sign-uo sign-in):endpoints Implement Sign up endpoint to persist data with postgres database Implement sign in endpoint to persist data with postgres database [delivers #165476156]

---
+++ server/dummyControllers.js/accountController.js
@@ -0,0 +1,90 @@
+import accounts from '../data/accounts';
+
+export default class AccountController {
+  /**
+    * @method createAccount
+    * @description Creates a new bank account
+    * @param {object} req - The Request Object
+    * @param {object} res - The Response Object
+    */
+  static createAccount(req, res) {
+    const { body: { initialDeposit, type } } = req;
+    const id = accounts.length + 1;
+    const accNoGen = Math.floor(Math.random() * 10000000000);
+    const accountNumber = accNoGen;
+    const owner = req.user.id;
+    const account = {
+      id,
+      accountNumber,
+      createdOn: new Date(),
+      owner,
+      type,
+      status: 'draft',
+      balance: parseFloat(initialDeposit)
+    };
+    accounts.push(account);
+    return res.status(201).json({
+      status: res.statusCode,
+      data: {
+        accountNumber: account.accountNumber,
+        firstname: req.user.firstname,
+        lastname: req.user.lastname,
+        email: req.user.email,
+        type: account.type,
+        openingBalance: account.balance,
+      },
+    });
+  }
+
+  /**
+    * @method accountStatus
+    * @description changes the account status by activating or deactivating it
+    * @param {object} req - The Request Object
+    * @param {object} res - The Response Object
+    */
+  static accountStatus(req, res) {
+    const { status } = req.body;
+    const { accountNumber } = req.params;
+    const validAccount = accounts.find(eachAccount => eachAccount.accountNumber === parseInt(accountNumber, 10));
+    if (validAccount) {
+      return res.status(200).json({
+        status: res.statusCode,
+        data: {
+          accountNumber,
+          status
+        }
+      });
+    }
+    return res.status(404).json({
+      status: res.statusCode,
+      error: `Account ${accountNumber} does not exist`
+    });
+  }
+
+  /**
+    * @method deleteAccount
+    * @description Deletes a specific account
+    * @param {object} req - The Request Object
+    * @param {object} res - The Response Object
+    */
+  static deleteAccount(req, res) {
+    const { accountNumber } = req.params;
+    const validAccount = accounts.find(eachAccount => eachAccount.accountNumber === parseInt(accountNumber, 10));
+    if (!validAccount) {
+      return res.status(404).json({
+        status: res.statusCode,
+        error: `Account ${accountNumber} does not exist`
+      });
+    }
+    accounts.forEach((account) => {
+      if (account.accountNumber === parseInt(accountNumber, 10)) {
+        const indexNumber = accounts.indexOf(account);
+        accounts.splice(indexNumber, 1);
+        return res.status(200).json({
+          status: res.statusCode,
+          msg: 'Account successfully deleted!'
+        });
+      }
+    });
+  }
+}
---
+++ server/dummyControllers.js/transactionControllers.js
@@ -0,0 +1,91 @@
+import transactions from '../data/transactions';
+import accounts from '../data/accounts';
+
+export default class TransactionController {
+  /**
+    * @method creditAccount
+    * @description credit a specific account
+    * @param {object} req - The Request Object
+    * @param {object} res - The Response Object
+    */
+  static creditAccount(req, res) {
+    const { amount } = req.body;
+    const { accountNumber } = req.params;
+    const validAccount = accounts.find(eachAccount => eachAccount.accountNumber === parseInt(accountNumber, 10));
+    if (!validAccount) {
+      return res.status(404).json({
+        status: res.statusCode,
+        error: `Account ${accountNumber} does not exist`
+      });
+    }
+
+    const transaction = {
+      id: transactions.length + 1,
+      createdOn: new Date(),
+      type: 'credit',
+      accountNumber: parseInt(accountNumber, 10),
+      cashier: req.user.id,
+      amount: parseFloat(amount),
+      oldBalance: validAccount.balance,
+      newBalance: parseFloat((validAccount.balance + parseFloat(amount)))
+    };
+
+    validAccount.balance = transaction.newbalance;
+    transactions.push(transaction);
+
+    return res.status(201).json({
+      status: res.statusCode,
+      data: {
+        transactionId: transaction.id,
+        accountNumber,
+        amount: transaction.amount,
+        cashier: transaction.cashier,
+        transactionType: transaction.type,
+        accountBalance: `Balance: ${transaction.newBalance}`
+      }
+    });
+  }
+
+  /**
+    * @method creditAccount
+    * @description credit a specific account
+    * @param {object} req - The Request Object
+    * @param {object} res - The Response Object
+    */
+  static debitAccount(req, res) {
+    const { amount } = req.body;
+    const { accountNumber } = req.params;
+    const validAccount = accounts.find(eachAccount => eachAccount.accountNumber === parseInt(accountNumber, 10));
+    if (!validAccount) {
+      return res.status(404).json({
+        status: res.statusCode,
+        error: `Account ${accountNumber} does not exist`
+      });
+    }
+    const transaction = {
+      id: transactions.length + 1,
+      createdOn: new Date(),
+      type: 'debit',
+      accountNumber: parseInt(accountNumber, 10),
+      cashier: req.user.id,
+      amount: parseFloat(amount),
+      oldBalance: validAccount.balance,
+      newBalance: parseFloat((validAccount.balance - parseFloat(amount)).toFixed(2))
+    };
+
+    validAccount.balance = transaction.newBalance;
+    transactions.push(transaction);
+
+    return res.status(201).json({
+      status: res.statusCode,
+      data: {
+        transactionId: transaction.id,
+        accountNumber,
+        amount: transaction.amount,
+        cashier: transaction.cashier,
+        transactionType: transaction.type,
+        accountBalance: `Balance: ${transaction.newBalance}`
+      }
+    });
+  }
+}
