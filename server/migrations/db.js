71870ad02a95fd73716a974f70a96786607b203a
obayomi96 <martinsoluwaseun47@gmail.com>
2019-04-28 21:16:59 +0100

    Fix test

--- server/migrations/db.js
+++ server/migrations/db.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connectionString = process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
-
+console.log('conStr', connectionString);
+console.log('nodeEnv', process.env.NODE_ENV);
 const client = new Pool({
   connectionString
 });
